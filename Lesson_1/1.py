36cfc825ffde08d3b4222bf1442f2ee38f028d2f
Urnguk <urnguk@gmail.com>
2026-02-10 11:39:44 +0300

    Update 1.py

--- Lesson_1/1.py
+++ Lesson_1/1.py
@@ -21,19 +21,38 @@
 # print("snoein")
 
 
-x = int(input())
-is_prime = True
-if x < 2:
-    is_prime = False
-else:
-    for div in range(2, int(x ** 0.5) + 1):
-        if x % div == 0:
-            is_prime = False
-            break
-if is_prime:
-    print(f'{x} is prime')
-else:
-    print(f"{x} is not prime")
+# x = int(input())
+# is_prime = True
+# if x < 2:
+#     is_prime = False
+# else:
+#     for div in range(2, int(x ** 0.5) + 1):
+#         if x % div == 0:
+#             is_prime = False
+#             break
+# if is_prime:
+#     print(f'{x} is prime')
+# else:
+#     print(f"{x} is not prime")
 
 
 # x / a = b => a * b = x => x / b = a
+
+
+# x = int(input())
+# is_prime = True
+
+# if x < 2:
+#     is_prime = False
+# else:
+#     div = 2
+#     while div ** 2 <= x:
+#         if x % div == 0:
+#             is_prime = False
+#             break
+#         div += 1
+
+# if is_prime:
+#     print(f'{x} is prime')
+# else:
+#     print(f"{x} is not prime")
